2414e25c8850bab5ccaab3bfa006eb90d00b66ce
eva <evamatejkova.e@gmail.com>
2023-04-24 08:32:43 +0200

    Added UI

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
-function game() {
-    let computerScore = 0;
-    let playerScore = 0;
-    let gameResult = '';
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Make a selection: rock x paper x scissors');
-        const computerSelection = getComputerChoice();
-        console.log('Computer: '+ computerSelection);
-        console.log('Player: '+ playerSelection);
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if (result == 1) {
-            playerScore++;
-            console.log('You win the round! ' + roundOutcomeLine(playerSelection, computerSelection));
-        } else if (result == 0){
-            computerScore++;
-            console.log('You lose the round! ' + roundOutcomeLine(computerSelection, playerSelection));
-        } else {
-            playerScore++;
-            computerScore++;
-            console.log('It\'s a tie! You both chose ' + computerSelection);
-        }
-    }
-    if (playerScore == computerScore) {
-        gameResult = 'The game is a tie!'
-    } else if (playerScore > computerScore) {
-        gameResult = 'You won the game!'
+let computerScore = 0;
+let playerScore = 0;
+let gameResult = '';
+
+const resultTextDiv = document.querySelector('#results');
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    const playerSelectionCase = playerSelection.toLowerCase();
+
+    if ((playerSelectionCase == 'rock' && computerSelection == 'paper')
+        || (playerSelectionCase == 'paper' && computerSelection == 'scissors')
+        || (playerSelectionCase == 'scissors' && computerSelection == 'rock')) {
+        computerScore++;
+        gameResult = 'You lose the round! ' + getOutcomeLine(computerSelection, playerSelection);
+
+    } else if ((playerSelectionCase == 'rock' && computerSelection == 'scissors')
+        || (playerSelectionCase == 'paper' && computerSelection == 'rock')
+        || (playerSelectionCase == 'scissors' && computerSelection == 'paper')) {
+        playerScore++;
+        gameResult = 'You win the round! ' + getOutcomeLine(playerSelection, computerSelection);
+        
     } else {
-        gameResult = 'You lost the game!'
+        playerScore++;
+        computerScore++;
+        gameResult = 'This round is a tie! You both chose ' + computerSelection + '.';
     }
-    return gameResult
+
+    if (computerScore == 5 && playerScore == 5) {
+        gameResult += '\r\nThe game is a tie!'
+        inactivateButtons();
+    } else if (computerScore == 5) {
+        gameResult += '\r\nYou have lost the game!'
+        inactivateButtons();
+    } else if (playerScore == 5) {
+        gameResult += '\r\nYou have won the game!'
+        inactivateButtons();
+    }
+
+    const resultText = document.querySelector('#results>.text');
+    resultText.setAttribute('style', 'white-space: pre;');
+    resultText.textContent = 'Player: ' + playerScore + ' | ' + 'Computer: ' + computerScore + ' | ';
+    resultText.textContent += gameResult;
+    return
 }
 
 function getComputerChoice() {
@@ -37,22 +50,7 @@ function getComputerChoice() {
     return choice
 }
 
-function playRound(playerSelection, computerSelection) {
-    let roundResult = -1; // -1 -> tie, 0 -> loss, 1 -> win
-    const playerSelectionCase = playerSelection.toLowerCase();
-    if ((playerSelectionCase == 'rock' && computerSelection == 'paper')
-    || (playerSelectionCase == 'paper' && computerSelection == 'scissors')
-    || (playerSelectionCase == 'scissors' && computerSelection == 'rock')) {
-        roundResult = 0;
-    } else if ((playerSelectionCase == 'rock' && computerSelection == 'scissors')
-    || (playerSelectionCase == 'paper' && computerSelection == 'rock')
-    || (playerSelectionCase == 'scissors' && computerSelection == 'paper')) {
-        roundResult = 1;
-    }
-    return roundResult
-}
-
-function roundOutcomeLine(winner, loser) {
+function getOutcomeLine(winner, loser) {
     const winnerFirstUppercase = winner.charAt(0).toUpperCase() + winner.slice(1);
     let verbForm = '';
     if (winner == 'scissors') {
@@ -63,5 +61,14 @@ function roundOutcomeLine(winner, loser) {
     return winnerFirstUppercase + ' ' + verbForm + ' ' + loser + '.'
 }
 
+function inactivateButtons() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    })
+}
 
-console.log(game());
+const buttons = document.querySelectorAll('.button');
+console.log(buttons);
+buttons.forEach((button) => button.addEventListener('click', () => {
+    playRound(button.value)
+}))
